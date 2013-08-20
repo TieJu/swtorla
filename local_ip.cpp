2c56db49e379d5594116c3085991a3d8d722f40d
Tiemo Jung <tiemo.jung@thm.de>
2013-08-21 01:43:46 +0200

    changed filtering to flags

--- local_ip.cpp
+++ local_ip.cpp
@@ -3,7 +3,7 @@
 
 #include <boost/scope_exit.hpp>
 
-std::vector<std::wstring> get_local_ip_addresses(u_long include_mask_ /*= IFF_UP*/, u_long exclude_mask_ /*= IFF_LOOPBACK | IFF_POINTTOPOINT*/) {
+std::vector<std::wstring> get_local_ip_addresses(glipa_flags flags_ /*= glipa_flags::none*/) {
     std::vector<std::wstring> result;
 
     auto sock = WSASocketW(AF_INET, SOCK_DGRAM, 0, 0, 0, 0);
@@ -35,11 +35,30 @@ std::vector<std::wstring> get_local_ip_addresses(u_long include_mask_ /*= IFF_UP
     std::vector<wchar_t> textbuffer;
 
     for ( auto& interface_info : buffer ) {
-        if ( !( interface_info.iiFlags & include_mask_ ) ) {
-            continue;
+        if ( ( flags_ & glipa_flags::include_inactive ) == glipa_flags::none ) {
+            if ( ( interface_info.iiFlags & IFF_UP ) == 0 ) {
+                continue;
+            }
         }
-        if ( interface_info.iiFlags & exclude_mask_ ) {
-            continue;
+        if ( ( flags_ & glipa_flags::include_loopback ) == glipa_flags::none ) {
+            if ( interface_info.iiFlags & IFF_LOOPBACK ) {
+                continue;
+            }
+        }
+        if ( ( flags_ & glipa_flags::include_p2p ) == glipa_flags::none ) {
+            if ( interface_info.iiFlags & IFF_POINTTOPOINT ) {
+                continue;
+            }
+        }
+        if ( ( flags_ & glipa_flags::require_broadcast ) != glipa_flags::none ) {
+            if ( ( interface_info.iiFlags & IFF_BROADCAST ) == 0 ) {
+                continue;
+            }
+        }
+        if ( ( flags_ & glipa_flags::require_multicast ) != glipa_flags::none ) {
+            if ( ( interface_info.iiFlags & IFF_MULTICAST ) == 0 ) {
+                continue;
+            }
         }
         DWORD length = 0;
         WSAAddressToStringW(reinterpret_cast<SOCKADDR*>( &interface_info.iiAddress ), sizeof( interface_info.iiAddress ), nullptr, nullptr, &length);
--- local_ip.h
+++ local_ip.h
@@ -4,7 +4,25 @@
 #include <string>
 #include <Ws2tcpip.h>
 
-// include_mask_: ored with INTERFACE_INFO::iiFlags if the result is zero the interface info will be ignored
-// exclude_mask_: ored with INTERFACE_INFO::iiFlags if the result is nonzero the interface info will be ignored
-// default is: only active interfaces, and no loopback and point2point interfaces
-std::vector<std::wstring> get_local_ip_addresses(u_long include_mask_ = IFF_UP, u_long exclude_mask_ = IFF_LOOPBACK | IFF_POINTTOPOINT);
+enum class glipa_flags {
+    none                = 0,
+    include_inactive    = 1 << 0,
+    include_loopback    = 1 << 1,
+    include_p2p         = 1 << 2,
+    require_broadcast   = 1 << 3,
+    require_multicast   = 1 << 4
+};
+
+inline glipa_flags operator|( glipa_flags lhs_, glipa_flags rhs_ ) {
+    return glipa_flags(unsigned( lhs_ ) | unsigned( rhs_ ));
+}
+
+inline glipa_flags operator&( glipa_flags lhs_, glipa_flags rhs_ ) {
+    return glipa_flags(unsigned( lhs_ ) & unsigned( rhs_ ));
+}
+
+inline glipa_flags operator^( glipa_flags lhs_, glipa_flags rhs_ ) {
+    return glipa_flags(unsigned( lhs_ ) ^ unsigned( rhs_ ));
+}
+
+std::vector<std::wstring> get_local_ip_addresses(glipa_flags flags_ = glipa_flags::none);
