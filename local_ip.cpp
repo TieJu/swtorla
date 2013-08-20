0fd5d7abcaae1fbebabceed806b3328e6951518c
Tiemo Jung <tiemo.jung@thm.de>
2013-08-21 01:25:10 +0200

    added some filter options to local ip lookup

--- local_ip.cpp
+++ local_ip.cpp
@@ -4,7 +4,7 @@
 
 #include <boost/scope_exit.hpp>
 
-std::vector<std::wstring> get_local_ip_addresses() {
+std::vector<std::wstring> get_local_ip_addresses(bool include_inactive_ /*= false*/, bool include_loopback_ /*= false*/, bool include_pointtopoint_ /*= false*/) {
     std::vector<std::wstring> result;
 
     auto sock = WSASocketW(AF_INET, SOCK_DGRAM, 0, 0, 0, 0);
@@ -35,7 +35,19 @@ std::vector<std::wstring> get_local_ip_addresses() {
 
     std::vector<wchar_t> textbuffer;
 
+    u_long skip_mask = 0;
+    skip_mask |= include_loopback_ ? 0 : IFF_LOOPBACK;
+    skip_mask |= include_pointtopoint_ ? 0 : IFF_POINTTOPOINT;
+    u_long inv_skip_mask = 0;
+    inv_skip_mask |= include_inactive_ ? 0 : IFF_UP;
+
     for ( auto& interface_info : buffer ) {
+        if ( !(interface_info.iiFlags & inv_skip_mask ) ) {
+            continue;
+        }
+        if ( interface_info.iiFlags & skip_mask ) {
+            continue;
+        }
         DWORD length = 0;
         WSAAddressToStringW(reinterpret_cast<SOCKADDR*>( &interface_info.iiAddress ), sizeof( interface_info.iiAddress ), nullptr, nullptr, &length);
         textbuffer.resize(length);
--- local_ip.h
+++ local_ip.h
@@ -3,4 +3,4 @@
 #include <vector>
 #include <string>
 
-std::vector<std::wstring> get_local_ip_addresses();
+std::vector<std::wstring> get_local_ip_addresses(bool include_inactive_ = false,bool include_loopback_ = false, bool include_pointtopoint_ = false);
